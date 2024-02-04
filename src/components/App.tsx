9df08af8c964a61c33b90c7b22de931912523209
uladzislau usovich <uladzislau.usovich@soft-fx.lv>
2024-02-04 23:22:02 +0400

    init based folder structure

---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+	return (
+		<div className="App">
+			<header>
+				<a
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					NBP Web API client
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;

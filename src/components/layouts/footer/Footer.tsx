49d1912641cbaad9d8977dd2035e6e54b7ce9cf2
uladzislau usovich <uladzislau.usovich@soft-fx.lv>
2024-03-06 00:17:47 +0400

    change footer text

--- src/components/layouts/footer/Footer.tsx
+++ src/components/layouts/footer/Footer.tsx
@@ -5,7 +5,7 @@ import S from "./Footer.styled";
 const Footer: React.FC = () => {
     return (
         <S.footer>
-            © 2024. Źródło śriednich kursów walut — Narodowy Bank Polski.
+            © 2024. Źródłem średnich kursów walut jest Narodowy Bank Polski.
         </S.footer>
     );
 };

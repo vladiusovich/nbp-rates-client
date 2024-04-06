8c3bcf844c54eb1396f608e8017263d0363262e8
uladzislau usovich <uladzislau.usovich@soft-fx.lv>
2024-04-06 16:39:24 +0400

    fix: fixed incorrect layout

--- src/components/layouts/Layout.styled.ts
+++ src/components/layouts/Layout.styled.ts
@@ -1,29 +1,25 @@
 /* eslint-disable max-len */
 import styled from "styled-components";
-import UI from "../uiKit";
 import device from "@device";
 
 const S = {
     container: styled.main`
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        box-sizing: border-box;
+        max-width: 1300px;
         min-height: 100vh;
+        box-sizing: border-box;
+        margin: 0 auto;
 
-		margin: 40px 20px;
-
-		@media ${device.lg} {
-			margin: 60px 350px;
-		}
+        @media ${device.xs}  {
+            margin: 40px 20px;
+        }
 
         @media ${device.sm}  {
             margin: 60px 48px;
         }
-    `,
 
-    content: styled(UI.Stack)`
-        position: relative;
+        @media ${device.lg} {
+            margin: 60px auto;
+		}
     `,
 };
 
--- src/components/pages/calculator/Calculator.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import UI from "@ui";
-
-const S = {
-	container: styled(UI.Grid)`
-	`,
-};
-
-export default S;
